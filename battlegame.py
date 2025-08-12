4351e28cf3176ee27a023610701b5f34ea13f0e1
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-08-12 14:37:59 +0300

    Task 1: Set Up the Game

---
+++ battlegame.py
@@ -0,0 +1,19 @@
+# Characters (as strings)
+wizard = "wizard"
+elf = "elf"
+human = "human"
+
+# Character HP (as integers):
+wizard_hp = 70
+elf_hp = 100
+human_hp = 150
+
+# Character Damage (as integers)
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
+
+# Dragon Stats (as integers)
+dragon_hp = 300
+dragon_damage = 50
+
