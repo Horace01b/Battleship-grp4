e275612d0b7fa26f4fe818f1c0fd200453cf5710
ianmabruk <ianmabruk3@gmail.com>
2025-08-12 15:23:28 +0300

    add new data

--- battlegame.py
+++ battlegame.py
@@ -17,12 +17,35 @@ human_damage = 20
 dragon_hp = 300
 dragon_damage = 50
 
+while True:
+    print("1.Wizard")
+    print("2.Elf")
+    print("3.Human")
 
-print("1.Wizard")
-print("2.Elf")
-print("3.Human")
+    character_choice = input("Choose your character: ")
 
-character_choice = input("Choose your character")
+    if character_choice=="1":
+        character=wizard
+        my_hp=wizard_hp
+        my_damage=wizard_damage
+        break
+    elif character_choice == "2":
+        character=elf
+        my_hp=elf_hp
+        my_damage=elf_damage
+        break
+    elif character_choice=="3":
+         character=human
+         my_hp=human_hp
+         my_damage=human_damage
+         break
+    else :
+        print("Uknown character")
+
+print(f"You have chosen the {character}!")
+print(f"HP: {my_hp}")
+print(f"Damage: {my_damage}")
+
 
 
 
